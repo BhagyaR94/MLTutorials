fbde771dc37bca0a347ec34276bf1833f6fa51e0
Bhagya Rathnayake <rathnayake.bhagya94@gmail.com>
2024-07-04 23:23:23 +0800

    one hot encoding example

---
+++ OneHotEncoding.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import  LabelBinarizer
+data = pd.read_csv('./datasets/one_hot_encoding/student_data.csv')
+
+result_category = data['results']
+
+encoder = LabelEncoder()
+result = encoder.fit_transform(result_category)
+
+binerizer = LabelBinarizer()
+result = binerizer.fit_transform(result)
+
+print(result)
+print(encoder.classes_)
