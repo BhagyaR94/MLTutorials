6dbf6852ae879ea8b3d9f88ab4f1d84d1ce41c86
Bhagya Rathnayake <rathnayake.bhagya94@gmail.com>
2024-07-07 11:07:47 +0800

    random forest practical

---
+++ RandomForest.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+data = pd.read_csv('./datasets/decision_tree/kyphosis.csv')
+x = data.drop('Kyphosis', axis=1)
+y = data['Kyphosis']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+
+model = RandomForestClassifier(n_estimators=50)  #n_estimators = number of decision trees
+model.fit(x_train, y_train)
+
+prediction = model.predict(x_test)
+print(prediction)
+print("\n")
+print(y_test)
+
+print(accuracy_score(prediction, y_test) * 100)
